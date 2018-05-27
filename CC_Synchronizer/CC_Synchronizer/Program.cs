8825c115fc2754726a5ca65fc54cedeb35021565
Christoph Posch <wi17b055@technikum-wien.at>
2018-05-28 00:56:20 +0200

    Create starter

---
+++ CC_Synchronizer/CC_Synchronizer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Synchronizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chocolate Costumizer Syncronizer\n\nPress any key to end.");
+            Console.ReadLine();
+
+            //try
+            //{
+            //    //ToDo: Start App-Provider                
+            //}
+            //catch (SocketException e)
+            //{
+
+            //}
+
+            //ToDO: Start Webfrontend-Provider
+        }
+    }
+}
