8ce7078c4ccfc222047d1e551edbd362cdd1b613
mkanyildiz <mkanyildiz@hotmail.com>
2018-06-13 18:32:23 +0200

    Added Shared Library with Model classes

--- CC_Synchronizer/SharedLibrary/Models/Product.cs
+++ CC_Synchronizer/SharedLibrary/Models/Product.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BackendDataHandler
+namespace SharedLibrary.Models
 {
-
     public class Product
     {
         public Product()
